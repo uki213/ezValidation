2ea897a69b95e348dc2960de11a0ad15df6baa27
Niimi <yuki_nimi@waku-2.com>
2015-09-14 19:27:07 +0900

    コメント追加

--- ezValidation.js
+++ ezValidation.js
@@ -48,7 +48,7 @@
                 $('#errBalloon' + index + ' *').remove();
                 if (msg.length !== 0) {
                     for (i = 0; i < msg.length; i = i + 1) {
-                        errMsgHtml = errMsgHtml + '<div>' + msg[i].replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, '') + '</div>';
+                        errMsgHtml = errMsgHtml + '<div>' + msg[i].replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, '') + '</div>'; // HTMLタグを除去してDOMを作成
                     }
                     $('#errBalloon' + index).html('<span class="balloon">' + errMsgHtml + closeBtnDom + '</span>');
                 }
